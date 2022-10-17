a3412d15d13b756325f929f3baf69ca5dca27d77
mikzanco <mikyzancolo97@gmail.com>
2022-10-17 10:04:49 +0200

    fix email problem: mancava una parentesi che mi bloccava l'uscita del prompt e quindi la possibilità di inserire l'email

--- mail/js/scirpt.js
+++ mail/js/scirpt.js
@@ -1,7 +1,7 @@
 // creo un array contenente diverse email.
 // creo un prompt per far scrivere le email
 // confronto l'email digitata con le email nell'array.
-const output = document.getElementById('output')
+// const output = document.getElementById('output')
 
 const email = ['mikyzancolo08@gmail.com', 'baubaumiciomicio@gmail.com', 'nonsonolatuaemail@gmail.com' ];
 
@@ -9,23 +9,25 @@ const emailSelected = prompt('Scrivi la tua email');
 
 let emailValida = false;
 
-if(email.includes(emailSelected)){
-    output.innerHTML = 'puoi accedere'
-         
-}else{
-    output.innerHTML = 'email not found'
-}
-
-// for(i = 0; i < email.length; i++){
-//     if(email[i] === emailSelected){
-//         emailValida = true;
-//         console.log('ok')
-//     }
-// }
-
-// if(emailValida == true{
+// if(email.includes(emailSelected)){
 //     output.innerHTML = 'puoi accedere'
-
+         
 // }else{
 //     output.innerHTML = 'email not found'
 // }
+
+let msg = '';
+
+for(let i = 0; i < email.length; i++){
+    if(email[i] === emailSelected){
+        emailValida = true;
+        console.log('ok')
+    }
+}
+
+if(emailValida == true){
+    output.innerHTML = 'puoi accedere'
+
+}else{
+    output.innerHTML = 'email not found'
+}
